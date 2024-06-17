97bc5d68d2818804e2352d92341f04f5ad59539c
Javohir <javohirblogger7@gmail.com>
2024-06-17 09:39:19 +0500

    Deploy

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from telethon import TelegramClient, events
 from config import API_ID, API_HASH, GROUP_ID
 from texts import ALL_WORDS
 
-# Funktsiya, regulyar ifoda orqali so'zni tekshiradi
+
 def check_words(text, words):
     return any(re.search(rf'\b\w*{word}\w*\b', text, re.IGNORECASE) for word in words)
 
@@ -13,10 +13,17 @@ client = TelegramClient('userbot', API_ID, API_HASH)
 async def handler(event):
     message = event.message
     if message.is_group and check_words(message.message, ALL_WORDS):
-        sender = await event.get_sender()
-        user_link = f'<a href="tg://user?id={sender.id}">{sender.first_name}</a>'
-        text_with_link = f"{message.message}\n\n👤 <b>{user_link}</b>"
-        await client.send_message(GROUP_ID, text_with_link, parse_mode='html')
+        try:
+            sender = await event.get_sender()
+            if sender:
+                user_link = f'<a href="tg://user?id={sender.id}">{sender.first_name}</a>'
+                text_with_link = f"{message.message}\n\n👤 <b>{user_link}</b>"
+                await client.send_message(GROUP_ID, text_with_link, parse_mode='html')
+            else:
+                text_with_link = f"{message.message}\n\n👤 <b>{sender.first_name}</b>"
+                await client.send_message(GROUP_ID, text=text_with_link, parse_mode='html')
+        except Exception as e:
+            print(f"Xato yuz berdi: {e}")
     elif message.is_private:
         await message.reply(message.message)
 
