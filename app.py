fde606cade386cf4368a5d65eaff91380a42e985
Javohir <javohirblogger7@gmail.com>
2024-06-17 20:38:09 +0500

    Deploy

--- app.py
+++ app.py
@@ -12,25 +12,29 @@ client = TelegramClient('userbot_new', API_ID, API_HASH)  # 'userbot_new' - yang
 @client.on(events.NewMessage)
 async def handler(event):
     message = event.message
-    if message.is_group and message.chat_id != GROUP_ID and check_words(message.message, ALL_WORDS):
-        await asyncio.sleep(5)  # 5 soniya kutish
-        try:
-            # Habarning hali ham mavjudligini tekshirish
-            await client.get_messages(message.chat_id, ids=message.id)
-        except:
-            return  # Agar habarning mavjud emasligi haqida xatolik bo'lsa, funksiyani to'xtatish
+    if message.is_group and message.chat_id != GROUP_ID:
+        if check_words(message.message, DONT_TAKE):
+            return  # Agar DONT_TAKE ichidagi so'zlar bo'lsa, funksiyani to'xtatish
         
-        chat_id = event.chat_id
-        message_id = message.id
-        if str(chat_id).startswith('-100'):
-            chat_id_str = str(chat_id)[4:]  # Remove '-100' prefix
-        else:
-            chat_id_str = str(chat_id)
-        
-        message_link = f"https://t.me/c/{chat_id_str}/{message_id}"
-        
-        text_with_link = f"{message.message}\n\n\n🔗 <a href='{message_link}'>Xabar havolasi</a>"
-        await client.send_message(GROUP_ID, message=text_with_link, parse_mode='html')
+        if check_words(message.message, ALL_WORDS):
+            await asyncio.sleep(5)  # 5 soniya kutish
+            try:
+                # Habarning hali ham mavjudligini tekshirish
+                await client.get_messages(message.chat_id, ids=message.id)
+            except:
+                return  # Agar habarning mavjud emasligi haqida xatolik bo'lsa, funksiyani to'xtatish
+            
+            chat_id = event.chat_id
+            message_id = message.id
+            if str(chat_id).startswith('-100'):
+                chat_id_str = str(chat_id)[4:]  # Remove '-100' prefix
+            else:
+                chat_id_str = str(chat_id)
+            
+            message_link = f"https://t.me/c/{chat_id_str}/{message_id}"
+            
+            text_with_link = f"{message.message}\n\n\n🔗 <a href='{message_link}'>Xabar havolasi</a>"
+            await client.send_message(GROUP_ID, message=text_with_link, parse_mode='html')
     elif message.is_private:
         await message.reply(message.message)
 
