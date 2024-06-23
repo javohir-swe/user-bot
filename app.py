43e6f1eb2697ad1b7d958f706d6a0ae0d0cb1533
Javohir <javohirblogger7@gmail.com>
2024-06-23 18:50:32 +0500

    Fix some problem

--- app.py
+++ app.py
@@ -7,13 +7,33 @@ from texts import ALL_WORDS, DONT_TAKE
 def check_words(text, words):
     return any(re.search(rf'\b\w*{word}\w*\b', text, re.IGNORECASE) for word in words)
 
+def contains_emoji(text):
+    emoji_pattern = re.compile(
+        r'[\U0001F600-\U0001F64F]|'  # Emoticons
+        r'[\U0001F300-\U0001F5FF]|'  # Symbols & Pictographs
+        r'[\U0001F680-\U0001F6FF]|'  # Transport & Map Symbols
+        r'[\U0001F700-\U0001F77F]|'  # Alchemical Symbols
+        r'[\U0001F780-\U0001F7FF]|'  # Geometric Shapes Extended
+        r'[\U0001F800-\U0001F8FF]|'  # Supplemental Arrows-C
+        r'[\U0001F900-\U0001F9FF]|'  # Supplemental Symbols and Pictographs
+        r'[\U0001FA00-\U0001FA6F]|'  # Chess Symbols
+        r'[\U0001FA70-\U0001FAFF]|'  # Symbols and Pictographs Extended-A
+        r'[\U00002702-\U000027B0]|'  # Dingbats
+        r'[\U000024C2-\U0001F251]|'  # Enclosed Characters
+        r'[\U0001F900-\U0001F9FF]|'  # Supplemental Symbols and Pictographs
+        r'[\U00002600-\U000026FF]',  # Miscellaneous Symbols
+        flags=re.UNICODE
+    )
+    return bool(emoji_pattern.search(text))
+
+
 client = TelegramClient('userbot_new', API_ID, API_HASH)  # 'userbot_new' - yangi session nomi
 
 @client.on(events.NewMessage)
 async def handler(event):
     message = event.message
     if message.is_group and message.chat_id != GROUP_ID:
-        if len(message.message) > 230:
+        if len(message.message) > 230 or contains_emoji(message.message):
             return  # Agar xabar 230 tadan uzun bo'lsa, funksiyani to'xtatish
 
         if check_words(message.message, DONT_TAKE):
